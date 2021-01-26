ebd3c88e0b44fe705e4805c33128153ca4541752
codeclanstudent <sstewart1993@outlook.com>
2021-01-26 15:40:00 +0000

    monsters

---
+++ models/monster.js
@@ -0,0 +1,8 @@
+const Monster = function(name, type, weakness, health){
+    this.name = name;
+    this.type = type;
+    this.weakness = weakness;
+    this.health = health;
+}
+
+module.exports = Monster;
--- models/witcher.js
+++ models/witcher.js
@@ -10,4 +10,31 @@ Witcher.prototype.add = function(trophy){
     this.trophies.push(trophy);
 }
 
+Witcher.prototype.remove = function(trophy){
+    this.trophies.pop(trophy);
+}
+
+Witcher.prototype.findHeaviestTrophy = function(){
+    let findHeaviestTrophy = this.trophies[0]
+
+    for (const trophy of this.trophies){
+        if (trophy.weight > findHeaviestTrophy.weight){
+            heaviestTrophy = trophy;
+        }
+    }
+    return heaviestTrophy;
+}
+
+Witcher.prototype.findLightestTrophy = function(){
+    let findLightestTrophy = this.trophies[0]
+
+    for (const trophy of this.trophies){
+        if (trophy.weight < findLightestTrophy.weight){
+            lightestTrophy = trophy;
+        }
+    }
+    return lightestTrophy;
+}
+
+
 module.exports = Witcher;
