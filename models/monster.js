8a0a8024013bb2b067dd97bd884000b2fd190ce8
codeclanstudent <sstewart1993@outlook.com>
2021-01-27 16:27:28 +0000

    attack

--- models/monster.js
+++ models/monster.js
@@ -1,8 +1,10 @@
-const Monster = function(name, type, weakness, health){
+const Monster = function(name, type, weakness, health, damage, loot){
     this.name = name;
     this.type = type;
     this.weakness = weakness;
     this.health = health;
+    this.damage = damage;
+    this.loot = loot;
 }
 
 module.exports = Monster;
--- models/witcher.js
+++ models/witcher.js
@@ -1,10 +1,12 @@
 const Monster = require("./monster");
 
-const Witcher = function (steel, silver, sign, ranged){
+const Witcher = function (steel, silver, sign, ranged, health, wallet){
     this.steel = steel;
     this.silver = silver;
     this.sign = sign;
     this.ranged = ranged;
+    this.health = health;
+    this.wallet = wallet
     this.trophies = []
 }
 
@@ -47,5 +49,36 @@ Witcher.prototype.battleSteel = function(witcher, monster){
     let attacks = monster.health / witcher.steel
     return attacks
 }
+
+Witcher.prototype.gettingLoot = function(witcher, monster){
+    let wallet = witcher.wallet + monster.loot 
+    return wallet
+}
+
+Witcher.prototype.buying = function(witcher, amount){
+    let wallet = witcher.wallet - amount 
+    return wallet
+}
+
+Witcher.prototype.steelAttack = function(witcher, monster){
+    let monsterHealth = monster.health - witcher.steel
+    return monsterHealth
+}
+
+Witcher.prototype.silverAttack = function(witcher, monster){
+    let monsterHealth = monster.health - witcher.silver
+    return monsterHealth
+}
+
+Witcher.prototype.rangedAttack = function(witcher, monster){
+    let monsterHealth = monster.health - witcher.ranged
+    return monsterHealth
+}
+
+Witcher.prototype.monsterAttack = function(witcher, monster){
+    let witcherHealth = witcher.health - monster.damage
+    return witcherHealth
+}
+
 
 module.exports = Witcher;
